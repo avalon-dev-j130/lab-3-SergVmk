0bd9c310fa1deff1a31b714217442d06b6986408
java <java@WS-248-06.avalon.ru>
2020-01-16 19:10:19 +0300

    Сделана 3 лаба

--- src/ru/avalon/java/actions/FileCopyAction.java
+++ src/ru/avalon/java/actions/FileCopyAction.java
@@ -1,5 +1,8 @@
 package ru.avalon.java.actions;
 
+import java.io.File;
+import java.nio.file.Files;
+
 /**
  * Действие, которое копирует файлы в пределах дискового
  * пространства.
@@ -13,7 +16,21 @@ public class FileCopyAction implements Action {
         /*
          * TODO №2 Реализуйте метод run класса FileCopyAction
          */
-        throw new UnsupportedOperationException("Not implemented yet!");
+        File filein = new File("c:/sk/1/forcopy.txt");
+        File filedest = new File("c:/sk/2/forcopy.txt");
+        try
+        {
+            Files.copy(filein.toPath(), filedest.toPath());
+        }
+        catch (Exception e)
+        {
+            //this.close();
+        }
+        
+        
+        
+        
+        
     }
 
     /**
@@ -24,6 +41,7 @@ public class FileCopyAction implements Action {
         /*
          * TODO №3 Реализуйте метод close класса FileCopyAction
          */
-        throw new UnsupportedOperationException("Not implemented yet!");
+        //Нет необходимости в текущей реализации
+        
     }
 }
